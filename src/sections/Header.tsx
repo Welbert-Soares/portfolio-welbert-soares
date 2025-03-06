89f6d9398f05ce3fcdf2ba8ce08f589a03b94671
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-03-06 00:17:44 -0300

    feat: Adicionar seção Hero com componentes e estilos dinâmicos

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,16 +1,10 @@
-'use client'
-
 import Link from 'next/link'
-import { useRouter, usePathname } from 'next/navigation'
 
 import { routes } from '@/constants/routes'
 
 export const Header = () => {
-  const pathName = usePathname()
-  console.log(pathName)
-
   return (
-    <div className="flex justify-center items-center relative top-3">
+    <div className="flex justify-center items-center fixed top-3 w-full">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         {routes.map(({ name, path }) => (
           <Link href={path} key={path} className="nav-item">
